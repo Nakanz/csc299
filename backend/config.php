b078f7bf39acfd43972cefe24e77cd75d9840c99
Nathan Hester <51456634+Nakanz@users.noreply.github.com>
2020-11-02 01:44:52 -1000

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Pauahi808');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'csc299');
  
 /* Attempt to connect to MySQL database */
